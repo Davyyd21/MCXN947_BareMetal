f9971d7a9577305f2a233c73cbc9b0a1fb799e7d
David_Munteanu <davmunteanu21@gmail.com>
2026-03-01 13:22:57 -0800

    Making startup_mcxn947.c more clear

--- startup_mcxn947.c
+++ startup_mcxn947.c
@@ -7,15 +7,12 @@ extern uint32_t _edata;
 extern uint32_t _sbss;
 extern uint32_t _ebss;
 extern uint32_t _stack_top;
-extern uint32_t _vStackBase;
 
-void Reset_Handler(void) __attribute__((naked, noreturn));
+void Reset_Handler(void);
 void SystemInit(void);
 extern int main(void);
 
-void Default_Handler(void) {
-    while (1);
-}
+void Default_Handler(void) { while (1); }
 
 void NMI_Handler(void)          __attribute__ ((weak, alias("Default_Handler")));
 void HardFault_Handler(void)    __attribute__ ((weak, alias("Default_Handler")));
@@ -30,31 +27,26 @@ void SysTick_Handler(void)      __attribute__ ((weak, alias("Default_Handler")))
 
 __attribute__((section(".isr_vector"), used))
 const uint32_t vectors[] = {
-    (uint32_t)&_stack_top,
-    (uint32_t)Reset_Handler,
-    (uint32_t)NMI_Handler,
-    (uint32_t)HardFault_Handler,
-    (uint32_t)MemManage_Handler,
-    (uint32_t)BusFault_Handler,
-    (uint32_t)UsageFault_Handler,
-    (uint32_t)SecureFault_Handler,
-    0, 0, 0,
-    (uint32_t)SVC_Handler,
-    (uint32_t)DebugMon_Handler,
-    0,
-    (uint32_t)PendSV_Handler,
-    (uint32_t)SysTick_Handler,
+    (uint32_t)&_stack_top,        /* 0: Initial Stack Pointer */
+    (uint32_t)Reset_Handler,      /* 1: Reset Handler */
+    (uint32_t)NMI_Handler,        /* 2: NMI Handler */
+    (uint32_t)HardFault_Handler,  /* 3: Hard Fault Handler */
+    (uint32_t)MemManage_Handler,  /* 4: MPU Fault Handler */
+    (uint32_t)BusFault_Handler,   /* 5: Bus Fault Handler */
+    (uint32_t)UsageFault_Handler, /* 6: Usage Fault Handler */
+    (uint32_t)SecureFault_Handler,/* 7: Secure Fault Handler */
+    0, 0, 0,                      /* 8-10: Reserved */
+    (uint32_t)SVC_Handler,        /* 11: SVCall Handler */
+    (uint32_t)DebugMon_Handler,   /* 12: Debug Monitor Handler */
+    0,                            /* 13: Reserved */
+    (uint32_t)PendSV_Handler,     /* 14: PendSV Handler */
+    (uint32_t)SysTick_Handler,    /* 15: SysTick Handler */
 };
 
-
-
-__attribute__((naked, noreturn))
+__attribute__((noreturn))
 void Reset_Handler(void)
 {
-    __asm volatile ("cpsid i");
-
     SCB->VTOR = (uint32_t)vectors;
-    __asm volatile ("MSR MSPLIM, %0" : : "r"(&_vStackBase));
 
     uint32_t *src = &_etext;
     uint32_t *dst = &_sdata;
@@ -69,8 +61,6 @@ void Reset_Handler(void)
 
     SystemInit();
 
-    __asm volatile ("cpsie i");
-
     main();
 
     while(1);
@@ -78,11 +68,10 @@ void Reset_Handler(void)
 
 void SystemInit(void) {
     WWDT0->MOD = 0; 
-
     SYSCON->ECC_ENABLE_CTRL = 0;
     CACHE64_CTRL0->CCR |= 1UL;
 
-    /* 3. FPU & PowerQuad */
+    // Enable FPU
     SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2) | (3UL << 0*2) | (3UL << 1*2));
 
     __DSB();
